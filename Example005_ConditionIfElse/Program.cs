6465d890f44da7b924d7e1be6d15eed0be267fc6
Akmaral.K <kadesheva2023@gmail.com>
2023-04-04 13:32:45 +0500

    Added Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Insert user's name:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurray, it is Masha!");
+}
+else
+{
+    Console.Write("Hi,");
+    Console.WriteLine(username);
+}
